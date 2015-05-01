0234206e169b239f542c96bec0f0729c0f23ee9f
Yannick Beynet <beynet@fresnes.info>
2015-05-01 15:46:49 +0200

    add custom query parser to handle accent in wild card query term

--- src/main/java/org/beynet/jnote/model/QueryParser.java
+++ src/main/java/org/beynet/jnote/model/QueryParser.java
@@ -2,12 +2,15 @@ package org.beynet.jnote.model;
 
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.TokenStream;
+import org.apache.lucene.analysis.core.KeywordTokenizer;
+import org.apache.lucene.analysis.miscellaneous.ASCIIFoldingFilter;
 import org.apache.lucene.analysis.tokenattributes.CharTermAttribute;
 import org.apache.lucene.index.Term;
 import org.apache.lucene.queryparser.classic.ParseException;
 import org.apache.lucene.search.Query;
 
 import java.io.IOException;
+import java.io.StringReader;
 
 /**
  * Created by beynet on 01/05/2015.
@@ -26,13 +29,15 @@ public class QueryParser extends org.apache.lucene.queryparser.classic.QueryPars
     protected Query getPrefixQuery(String field, String termStr) throws ParseException {
         String newTerm=termStr;
         try {
-            TokenStream tokenStream = getAnalyzer().tokenStream(field, termStr);
+            StringReader reader = new StringReader(termStr);
+            KeywordTokenizer input = new KeywordTokenizer(termStr.length()*2);
+            input.setReader(reader);
+            TokenStream tokenStream = new ASCIIFoldingFilter(input);
             CharTermAttribute charTermAttribute = tokenStream.addAttribute(CharTermAttribute.class);
             tokenStream.reset();
 
             while (tokenStream.incrementToken()) {
                 newTerm = charTermAttribute.toString();
-                break;
             }
             tokenStream.close();
         } catch (IOException e) {
