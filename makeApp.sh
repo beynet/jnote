7c72cb74d08f13fc02861fd41d115d6bf0551a84
Yannick Beynet <beynet@fresnes.info>
2017-10-21 15:45:50 +0200

    next version 1.0.11

--- makeApp.sh
+++ makeApp.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-$JAVA_HOME/bin/javapackager -deploy -native image -srcfiles ./target/jnote-1.0.10-jar-with-dependencies.jar -appclass org.beynet.jnote.gui.Main -name jnote -outdir ./target/app -outfile jnote
+$JAVA_HOME/bin/javapackager -deploy -native image -srcfiles ./target/jnote-1.0.11-SNAPSHOT-jar-with-dependencies.jar -appclass org.beynet.jnote.gui.Main -name jnote -outdir ./target/app -outfile jnote
