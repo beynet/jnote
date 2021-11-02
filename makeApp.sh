253304dec2a5914692d0bd31c12cf80d22716945
Yannick Beynet <beynet@fresnes.info>
2021-11-02 11:46:52 +0100

    correct build for mac

--- makeApp.sh
+++ makeApp.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk-14.0.1.jdk/Contents/Home
+export JAVA_HOME=/Library/Java/JavaVirtualMachines/zulu17.30.15-ca-jdk17.0.1-macosx_x64/zulu-17.jdk/Contents/Home
+export JPACKAGE=/Library/Java/JavaVirtualMachines/zulu17.30.15-ca-jdk17.0.1-macosx_x64/zulu-17.jdk/Contents/Home/bin/jpackage
 
 mvn clean package -Dmaven.test.skip=true && \
-$JAVA_HOME/bin/jpackage --runtime-image $JAVA_HOME  -n jnote --main-class org.beynet.jnote.gui.Main --main-jar jnote-1.1.1-jar-with-dependencies.jar --input ./target
+$JPACKAGE --runtime-image $JAVA_HOME -n jnote --main-class org.beynet.jnote.gui.Main --main-jar jnote-1.1.2-SNAPSHOT-jar-with-dependencies.jar --input ./target
